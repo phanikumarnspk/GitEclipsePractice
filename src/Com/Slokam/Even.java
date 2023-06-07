cdf0e5ecdb98298c934449b3056168c16bdb19b3
praveen <pchary59@gmail.com>
2023-06-07 20:31:48 +0530

    java commit

---
+++ src/Com/Slokam/Even.java
@@ -0,0 +1,17 @@
+package Com.Slokam;
+
+import java.util.Arrays;
+
+public class Even {
+	
+	public static void main(String[] args) {
+	
+	for(int i=0; i<20; i++)
+	{
+		if(i%2==0)
+		{
+			System.out.println(i);
+		}
+	}
+}
+}
