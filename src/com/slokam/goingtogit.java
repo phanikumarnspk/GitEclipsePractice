157fde4f040f7b51e722ef29f1ad14c16e5874d1
trinadh <trinadhbommineedu@gmail.com>
2023-06-08 12:44:14 +0530

    hello

---
+++ src/com/slokam/goingtogit.java
@@ -0,0 +1,12 @@
+package com.slokam;
+
+public class goingtogit {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("going to git hub");
+		
+		
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Yes
+ *
+ */
+module git_repo_to_git_hub {
+}
