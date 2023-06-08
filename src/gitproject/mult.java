7d8c723fc0b55e72659d60de5e191a80e22033fd
shivateja <shiva.tejas99@gmail.com>
2023-06-08 11:41:32 +0530

    java commit

---
+++ src/gitproject/mult.java
@@ -0,0 +1,16 @@
+package gitproject;
+
+public class mult {
+
+	public static void main(String[] args) {
+		
+		int a = 11;
+		int b = 15;
+		int c = a*b;
+		System.out.println("a = "+a);
+		System.out.println("b = "+b);
+		System.out.println("c = a*b = "+c);
+		
+	}
+
+}
