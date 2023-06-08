9f4a3ebf30e1de3b232e7bfcd8a9894e848399ea
Rakesh <rakesh.burri2000.com>
2023-06-08 12:51:21 +0530

    second commit

---
+++ src/adding/addingAndSub.java
@@ -0,0 +1,15 @@
+package adding;
+
+public class addingAndSub {
+
+	public static void main(String[] args) {
+		int a=13;
+		int b=14;
+		int c=a+b;
+		int e=a-b;
+		System.out.println(c);
+		System.out.println(e);
+
+	}
+
+}
