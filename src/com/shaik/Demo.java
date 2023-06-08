1fb6f23d4c50667639233334bcc21490e1659833
Saidashaik786 <108187239+Saidashaik786@users.noreply.github.com>
2023-06-08 12:36:03 +0530

    git practice

---
+++ src/com/shaik/Demo.java
@@ -0,0 +1,14 @@
+package com.shaik;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		
+		int arr[]= {10,30,60,67,87,89};
+		for(int i=0;i<=arr.length-1;i++) {
+			System.out.println(arr[i]);
+		}
+	}
+}
