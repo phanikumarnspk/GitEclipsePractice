9e9428028c579b26c66c114fafa6362a0b17ddac
BIYYANI ARUN KUMAR REDDY <arunreddy453@gmail.com>
2023-06-08 12:34:23 +0530

    git practice eclipse

---
+++ src/gitpracticeeclipse/git.java
@@ -0,0 +1,13 @@
+package gitpracticeeclipse;
+
+public class git {
+	
+	public static void main(String[] args) {
+		
+		double k = 789.36;
+		double v = 743.16;
+		double a = k + v;
+		System.out.println(a);
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Yadava Reddy Gandhar
+ *
+ */
+module gitpracticeeclipse {
+}
