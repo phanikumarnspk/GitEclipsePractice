2b7d19d6709ae5cdc21f530d8e560ef57df16354
Lokesh <lokeshbandaru3@gmail.com>
2023-06-08 16:22:58 +0530

    first commit

---
+++ project/first.java
@@ -0,0 +1,17 @@
+package project;
+
+public class first {
+public void m1() {
+	System.out.println("this is m1 method");
+}
+public void m2() {
+	System.out.println("this is m2 method");
+	
+}
+public static void main(String[] args) {
+	first f1=new first();
+	f1.m1();
+	f1.m2();
+}
+}
+
