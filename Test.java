727a4a452eeb4413721a2a2fe33db652df32a9b3
MadishettiPrashanth <prashanthmadishetti1999@gmail.com>
2023-06-08 23:51:17 +0530

    hii

---
+++ Test.java
@@ -0,0 +1,17 @@
+
+public class Test {
+	
+	public void m1()  {
+		
+		System.out.println("Hello");
+	}
+
+	public static void main(String[] args) {
+		
+		Test t=new Test();
+		t.m1();
+		
+
+	}
+
+}
