2e04a52cd5240d114923f8115df55888219514b1
ramu <ramu25251944@gmail.com>
2023-06-09 13:04:09 +0530

    hello every one

---
+++ eclipseone/slokam/src/slokam/array.java
@@ -0,0 +1,22 @@
+package slokam;
+
+public class array {
+
+	public void m1()
+	{
+	int arr[] = {20,45,63,89,25};
+	for(int i=0;i<=4;i++)
+	{
+		System.out.println(arr[i]);
+	}
+	
+	}
+	
+	
+	public static void main(String[] args) {
+		
+    array a = new array();
+    a.m1();
+	}
+
+}
