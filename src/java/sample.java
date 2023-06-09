76e6890f51a7ff0df4c3ba7b785a3cd14289ce3f
Shaikrizwan <Shaikrizwanbasha8639@gmail.com>
2023-06-09 11:05:30 +0530

    sample

---
+++ src/java/sample.java
@@ -0,0 +1,11 @@
+package java;
+
+public class sample {
+public void programming() {
+	System.out.println("my first programme");
+}
+public static void main(String[] args) {
+	sample s=new sample();
+	s.programming();
+}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author ShaikRizwan
+ *
+ */
+module java {
+}
